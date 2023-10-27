011d33c5c0bbdda5d04ce94d7bba99f8ef2ae837
Alexander Turiak <oturiak@adobe.com>
2023-10-27 22:43:31 +0200

    First version of Color Spectrum

--- Teams/white_dodgers/Java/src/ColorSpectrum.java
+++ Teams/white_dodgers/Java/src/ColorSpectrum.java
@@ -8,7 +8,7 @@ import java.util.Random;
 
 public class ColorSpectrum {
 
-    private static final String host = "host.docker.internal";
+    private static final String host = "127.0.0.1";
     private static final int port = 1234;
     private static final List<String> colors = List.of("00ff00", "e28211", "ff0000", "99b259", "0000ff");
     private static final int xOffset = 10;
@@ -29,14 +29,20 @@ public class ColorSpectrum {
             final String[] answer = reader.readLine().split(" ");
             System.out.printf("Detected size: %s x %s%n", answer[1], answer[2]);
 
+            int xFieldWidth = (Integer.parseInt(answer[1]) / 3) / 2;
+            int yFieldWidth = (Integer.parseInt(answer[2]) / 3) / 2;
+
+             System.out.printf("Our field size: %s x %s%n", xFieldWidth, yFieldWidth);
+
             System.out.println("Painting...");
             StringBuilder out = new StringBuilder();
-            for (int x = 0; x < 500; x++) {
+            for (int x = 725; x < 725 + xFieldWidth; x++) {
                 Random random = new Random();
                 int nextInt = random.nextInt(0xffffff + 1);
                 String colorCode = String.format("#%06x", nextInt);
-                for (int y = 0; y < 200; y++) {
+                for (int y = 5; y < 5 + yFieldWidth; y++) {
                     out.append(String.format("PX %d %d %s\n", x, y, colorCode.replace("#", "")));
+                    System.out.printf("Paining pixel at %s x %s%n", x, y);
                 }
             }
             writer.print(out);
